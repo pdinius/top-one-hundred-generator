ca6c8063f0f720d01fe7fcc78c323721a0fcec03
dinx <phildinius@gmail.com>
2022-11-29 14:03:08 -0500

    Added copy to clipboard

--- src/components/RankGames/RankedGamesList/RankedGamesList.tsx
+++ src/components/RankGames/RankedGamesList/RankedGamesList.tsx
@@ -7,7 +7,13 @@ import RankedGamesListItem from './RankedGamesListItem/RankedGamesListItem';
 const RankedGamesList: FC<RankedGamesListProps> = ({ games }) => {
   const gameItems: Array<Game> = games.map(g => g.pivot);
 
+  const downloadGameList = () => {
+    const gameList = games.map(g => g.pivot.name).join('\n');
+    navigator.clipboard.writeText(gameList);
+  }
+
   return <div className={styles.gameListContainer}>
+    <button onClick={downloadGameList} className={styles.exportButton}>copy games to clipboard</button>
     {gameItems.map((g,i) => <RankedGamesListItem key={g.name} game={g} rank={i+1} />)}
   </div>;
 };
