f55c63197040e3099e93ca4e876e3034697a1602
dinx <phildinius@gmail.com>
2022-11-30 17:34:10 -0500

    Added glow

--- src/components/RankGames/RankGamesProgress/RankGamesProgressSection/RankGamesProgressSection.tsx
+++ src/components/RankGames/RankGamesProgress/RankGamesProgressSection/RankGamesProgressSection.tsx
@@ -3,6 +3,8 @@ import styles from './RankGamesProgressSection.module.scss';
 import { RankGamesProgressSectionProps } from './RankGamesProgressSection.props';
 
 const RankGamesProgressSection: FC<RankGamesProgressSectionProps> = ({ width, current, title }) => {
+  const content = current === 'main' ? (width - 4) / 2 + 1 : width / 2;
+
   let classes = [styles.progressSection];
   switch (current) {
     case 'main':
@@ -11,8 +13,8 @@ const RankGamesProgressSection: FC<RankGamesProgressSectionProps> = ({ width, cu
     case 'side':
       classes.push(styles.sideSection);
   }
+  if (!/^\d+ Games$/.test(title)) classes.push(styles.titleSection)
 
-  const content = current === 'main' ? (width - 4) / 2 + 1 : width / 2;
 
   return <div style={{ width: `${width}px` }} className={classes.join(' ')} title={title}>{width >= 10 ? content : ''}</div>
 };
